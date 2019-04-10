f8709e20e6eb3714798cf33fa5533acb509e4a58
vaglarov <pasific@abv.bg>
2019-04-10 22:02:09 +0300

    final exam

---
+++ FinalExam/28 October 2018 Part II/vs/28 October 2018 Part II/03. Chore Wars_Условия/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _03._Chore_Wars_Условия
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = string.Empty;
+            var dictTime = new Dictionary<string, int>();
+            dictTime.Add("Doing the dishes", 0);
+            dictTime.Add("Cleaning the house", 0);
+            dictTime.Add("Doing the laundry", 0);
+
+   
+            while ((command = Console.ReadLine()) != "wife is happy")
+            {
+                string pattern = @"(?<dish><[a-z0-9]+>)|(?<cleaning>\[[A-Z0-9]+\])|(?<laundry>\{[^\s\t]+})";
+                var resultRegex = Regex.Matches(command, pattern);
+                foreach (Match match in resultRegex)
+                {
+                    if (match.Groups["dish"].Value!="")
+                    {
+                        var patternSmallLetters = @"[a-z]";
+                        var stringWithLetters=match.Groups["dish"].Value;
+                        stringWithLetters=stringWithLetters.TrimStart('<');
+                        stringWithLetters = stringWithLetters.TrimEnd('>');
+                        var resultTime = Regex.Replace(stringWithLetters, patternSmallLetters, "");
+                        for (int i = 0; i < resultTime.Length; i++)
+                        {
+                            var digits = int.Parse(resultTime[i].ToString());
+                        dictTime["Doing the dishes"] += digits;
+                        }
+                    }
+                    else if (match.Groups["cleaning"].Value != "")
+                    {
+                        var patternOnlyDigits = @"[^\d]";
+                        var stringWithLetters = match.Groups["cleaning"].Value;
+                        var resultTime = Regex.Replace(stringWithLetters, patternOnlyDigits, "");
+                        for (int i = 0; i < resultTime.Length; i++)
+                        {
+                            var digits = int.Parse(resultTime[i].ToString());
+                        dictTime["Cleaning the house"] += digits;
+                        }
+
+                    }
+                    else if (match.Groups["laundry"].Value != "")
+                    {
+                        var patternUpperLetters = @"[^\d]";
+                        var stringWithLetters = match.Groups["laundry"].Value;
+                        stringWithLetters = stringWithLetters.TrimStart('[');
+                        stringWithLetters = stringWithLetters.TrimEnd(']');
+                        var resultTime = Regex.Replace(stringWithLetters, patternUpperLetters, "");
+                        for (int i = 0; i < resultTime.Length; i++)
+                        {
+                            var digits = int.Parse(resultTime[i].ToString());
+                        dictTime["Doing the laundry"] += digits;
+                        }
+                    }
+                }
+
+               
+            }
+       
+            Console.WriteLine($"Doing the dishes - {dictTime["Doing the dishes"]} min.");
+            Console.WriteLine($"Cleaning the house - {dictTime["Cleaning the house"]} min.");
+            Console.WriteLine($"Doing the laundry - {dictTime["Doing the laundry"]} min.");
+            Console.WriteLine($"Total - {dictTime.Values.Sum()} min.");
+        }
+    }
+}
---
+++ FinalExam/28 October 2018 Part II/vs/28 October 2018 Part II/04. MeTube Statistics/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _04._MeTube_Statistics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string lineInput = string.Empty;
+            var dictSongViews = new Dictionary<string, int>();
+            var dictSongLikes = new Dictionary<string, int>();
+            while ((lineInput = Console.ReadLine()) != "stats time")
+            {
+                string pattern = @"(like:)|(dislike:)";
+                var songName = string.Empty;
+                if (Regex.IsMatch(lineInput, pattern))
+                {
+                    var patterLike = @"dislike:";  
+                    if (Regex.IsMatch(lineInput, patterLike))
+                    {
+                        var splitInput = lineInput.Split(":");
+                        songName = splitInput[1];
+                        if (dictSongViews.ContainsKey(songName))
+                        {
+                            dictSongLikes[songName] -= 1;
+                        }
+                    }
+                    else 
+                    {
+                        var splitInput = lineInput.Split(":");
+                        songName = splitInput[1];
+                        if (dictSongViews.ContainsKey(songName))
+                        {
+                            dictSongLikes[songName] += 1;
+                        }
+                      
+                    }
+                }
+                else
+                {
+                    var splitInput = lineInput.Split("-");
+                    songName = splitInput[0];
+                    var veiwNumber = int.Parse(splitInput[1]);
+                    if (!dictSongViews.ContainsKey(songName))
+                    {
+                        dictSongViews.Add(songName, 0);
+                        dictSongLikes.Add(songName, 0);
+                    }
+                    dictSongViews[songName] += veiwNumber;
+                }
+
+            }
+            lineInput = Console.ReadLine();
+            if (lineInput == "by views")
+            {
+                foreach (var kvp in dictSongViews.OrderByDescending(x => x.Value))
+                {
+                    Console.WriteLine($"{kvp.Key} - {kvp.Value} views - {dictSongLikes[kvp.Key]} likes");
+                }
+            }
+            else
+            {
+                foreach (var kvp in dictSongLikes.OrderByDescending(x => x.Value))
+                {
+                    Console.WriteLine($"{kvp.Key} - {dictSongViews[kvp.Key]} views - {kvp.Value} likes");
+                }
+            }
+        }
+    }
+}

2ebe2de75533f3795c28da914df1f26a0f8522b9
vaglarov <pasific@abv.bg>
2019-04-12 21:43:43 +0300

    01072018

---
+++ FinalExam/01 July 2018 Part II/vs/01 July 2018 Part II/3 – SoftUni Bar Income/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _3___SoftUni_Bar_Income
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = string.Empty;
+            double totalIncome = 0;
+            while ((line = Console.ReadLine()) != "end of shift")
+            {
+                string patternRegex = @"(^%(?<name>[A-Z][a-z]+)%)([^\|\$\%\.\d]*)(\<(?<product>\w+)\>)([^\|\$\%\.\d]*)(\|(?<count>\d+)\|)([^\|\$\%\.\d]*)((?<price>\d*(\.\d+)?)\$)";
+                if (Regex.IsMatch(line, patternRegex))
+                {
+                    var regex = Regex.Match(line, patternRegex);
+                    var name = regex.Groups["name"].Value;
+                    var product = regex.Groups["product"].Value;
+                    var count = int.Parse(regex.Groups["count"].Value);
+                    var price = double.Parse(regex.Groups["price"].Value);
+                    var totalProduct = count * price;
+                    totalIncome += totalProduct;
+                    Console.WriteLine($"{name}: {product} - {totalProduct:f2}");
+                }
+
+            }
+            Console.WriteLine($"Total income: {totalIncome:f2}");
+        }
+    }
+}

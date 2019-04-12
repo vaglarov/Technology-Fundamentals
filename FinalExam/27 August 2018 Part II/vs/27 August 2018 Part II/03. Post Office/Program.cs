54b0698cd65a67b9d471103d902726821fc2dac3
vaglarov <pasific@abv.bg>
2019-04-12 19:22:48 +0300

    100\100

---
+++ FinalExam/27 August 2018 Part II/vs/27 August 2018 Part II/03. Post Office/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _03._Post_Office
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split('|');
+            var firstPart = input[0];
+            var secondPart = input[1];
+            var thirdPart = input[2];
+            string patterFirstPart = @"([#$%*&])(?<UpperLetters>[A-Z]+)\1";
+            string patternSecondPart = @"\d{2}:\d{2}";
+            var upperLetter = Regex.Match(firstPart, patterFirstPart).Groups["UpperLetters"];
+            var matchNumbers = Regex.Matches(secondPart, patternSecondPart);
+            foreach (var matchSymbol in upperLetter.ToString())
+            {
+                int symbolFirstPart = matchSymbol;
+                foreach (var match in matchNumbers)
+                {
+                    var matchSplit = match.ToString().Split(':');
+                    var asciiSymbol = int.Parse(matchSplit[0]);
+                    var numberSymbols = int.Parse(matchSplit[1]);
+                    var symbolSecondPart = asciiSymbol;
+                    if (symbolSecondPart==symbolFirstPart)
+                    {
+                        foreach (var item in thirdPart.Split())
+                        {
+                            if (item.StartsWith((char)symbolFirstPart)&&item.Length==numberSymbols+1)
+                            {
+                                Console.WriteLine(item);
+                                break;
+                            }
+                            
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ FinalExam/27 August 2018 Part II/vs/27 August 2018 Part II/04. Iron Girder/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Iron_Girder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputLine = string.Empty;
+            var dictTownTime = new Dictionary<string, int>();
+            var dictTownPeople = new Dictionary<string, int>();
+            while ((inputLine = Console.ReadLine()) != "Slide rule")
+            {
+                var commands = inputLine.Split(new string[] { ":", "->" }, StringSplitOptions.RemoveEmptyEntries);
+                var town = commands[0];
+                var countPeople = int.Parse(commands[2]);
+                var timeOrAmbush = commands[1];
+                if (timeOrAmbush != "ambush")
+                {
+                    var time = int.Parse(timeOrAmbush);
+                    if (!dictTownPeople.ContainsKey(town))
+                    {
+                        dictTownPeople.Add(town, 0);
+                        dictTownTime.Add(town, 0);
+                    }
+                    if (dictTownTime[town] > time || dictTownTime[town]==0)
+                    {
+                        dictTownTime[town] = time;
+                    }
+                  
+                    dictTownPeople[town] += countPeople;
+                }
+                else if (dictTownPeople.ContainsKey(town))
+                {
+
+                    dictTownTime[town] = 0;
+                    dictTownPeople[town] -= countPeople;
+
+                    if (dictTownPeople[town] <= 0)
+                    {
+                        dictTownPeople.Remove(town);
+                        dictTownTime.Remove(town);
+                    }
+                }
+
+            }
+            //print
+            foreach (var kvp in dictTownTime.Where(x => x.Value > 0).OrderBy(x => x.Value).ThenBy(x => x.Key))
+            {
+                Console.WriteLine($"{kvp.Key} -> Time: {kvp.Value} -> Passengers: {dictTownPeople[kvp.Key]}");
+            }
+        }
+    }
+}

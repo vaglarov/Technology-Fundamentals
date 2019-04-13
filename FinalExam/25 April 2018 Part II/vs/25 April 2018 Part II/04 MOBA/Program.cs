df9c438ee05d661e09961f09dae0215ef99e69a4
vaglarov <pasific@abv.bg>
2019-04-13 10:54:56 +0300

    25 04 2018

---
+++ FinalExam/25 April 2018 Part II/vs/25 April 2018 Part II/04 MOBA/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04_MOBA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+            var dictPlayer = new Dictionary<string, Dictionary<string, int>>();
+            while ((input = Console.ReadLine()) != "Season end")
+            {
+                var command = input.Split(new string[] { " -> ", " vs " }, StringSplitOptions.RemoveEmptyEntries);
+                //Add in dict
+                if (command.Length == 3)
+                {
+                    var nameToAdd = command[0];
+                    var gameToAdd = command[1];
+                    var pointToAdd = int.Parse(command[2]);
+                    if (!dictPlayer.ContainsKey(nameToAdd))
+                    {
+                        dictPlayer.Add(nameToAdd, new Dictionary<string, int>());
+                    }
+                    if (!dictPlayer[nameToAdd].ContainsKey(gameToAdd))
+                    {
+                        dictPlayer[nameToAdd].Add(gameToAdd, pointToAdd);
+                    }
+                    else if (dictPlayer[nameToAdd][gameToAdd] < pointToAdd)
+                    {
+                        dictPlayer[nameToAdd][gameToAdd] = pointToAdd;
+                    }
+                }
+                else
+                {
+                    var nameFirstPlayer = command[0];
+                    var nameSecondPlayer = command[1];
+                    if (dictPlayer.ContainsKey(nameFirstPlayer) && dictPlayer.ContainsKey(nameSecondPlayer))
+                    {
+                        foreach (var kvpFirstPlayer in dictPlayer[nameFirstPlayer])
+                        {
+                            if (dictPlayer[nameSecondPlayer].ContainsKey(kvpFirstPlayer.Key))
+                            {
+                                var firstTotalPoint = dictPlayer[nameFirstPlayer].Values.Sum();
+                                var secondTotalPoint = dictPlayer[nameSecondPlayer].Values.Sum();
+                                if (firstTotalPoint > secondTotalPoint)
+                                {
+                                    dictPlayer.Remove(nameSecondPlayer);
+                                    break;
+                                }
+                                else if (secondTotalPoint>firstTotalPoint)
+                                {
+                                    dictPlayer.Remove(nameFirstPlayer);
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            //print
+            foreach (var kvp in dictPlayer.OrderByDescending(x => x.Value.Values.Sum()).ThenBy(x => x.Key))
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value.Values.Sum()} skill");
+                foreach (var item in kvp.Value.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+                {
+                    Console.WriteLine($"- {item.Key} <::> {item.Value}");
+                }
+            }
+        }
+    }
+}
+
---
+++ FinalExam/25 April 2018 Part II/vs/25 April 2018 Part II/3 – Tseam Account/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3___Tseam_Account
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var listGames = new List<string>();
+            var fistLineGame = Console.ReadLine().Split(' ');
+            //Add in List
+            foreach (var game in fistLineGame)
+            {
+                if (!listGames.Contains(game))
+                {
+                    listGames.Add(game);
+                }
+            }
+            var input = string.Empty;
+            while ((input=Console.ReadLine())!= "Play!")
+            {
+                var commans = input.Split(' ');
+                switch (commans[0])
+                {
+                    case "Install":
+                        var game = commans[1];
+                        if (!listGames.Contains(game))
+                        {
+                            listGames.Add(game);
+                        }
+                        break;
+                    case "Uninstall":
+                        game = commans[1];
+                        if (listGames.Contains(game))
+                        {
+                            listGames.Remove(game);
+                        }
+                        break;
+                    case "Update":
+                        game = commans[1];
+                        if (listGames.Contains(game))
+                        {
+                            listGames.Remove(game);
+                            listGames.Add(game);
+                        }
+                        break;
+                    case "Expansion":
+                        var gameToExpansion = commans[1].Split('-');
+                        game = gameToExpansion[0];
+                        var gameWihtExpansion = game + ':' + gameToExpansion[1];
+                        if (listGames.Contains(game))
+                        {
+                            var index=listGames.IndexOf(game);
+                            listGames.Insert(index + 1, gameWihtExpansion);
+                        }
+                        break;
+                }
+            }
+
+            foreach (var item in listGames)
+            {
+                Console.Write($"{item} ");
+            }
+        }
+    }
+}

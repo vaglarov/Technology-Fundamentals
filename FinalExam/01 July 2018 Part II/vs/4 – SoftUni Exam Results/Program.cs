f2a78a7f8aa9722f54bb65f1bdf0ffdff83dde82
vaglarov <pasific@abv.bg>
2019-04-13 08:46:49 +0300

    4 – SoftUni Exam Results

---
+++ FinalExam/01 July 2018 Part II/vs/4 – SoftUni Exam Results/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4___SoftUni_Exam_Results
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+            var dictUsers = new Dictionary<string, int>();
+            var dictLang = new Dictionary<string, int>();
+            while ((input=Console.ReadLine())!= "exam finished")
+            {
+                var command = input.Split('-');
+                if (command.Length==3)
+                {
+                    //Pesho-Java-91
+                    var nameUsers = command[0];
+                    var language = command[1];
+                    var pointSubmit = int.Parse(command[2]);
+                    //add User
+                    if (!dictUsers.ContainsKey(nameUsers))
+                    {
+                        dictUsers.Add(nameUsers, pointSubmit);
+                    }
+                    else if (dictUsers[nameUsers]<pointSubmit)
+                    {
+                        dictUsers.Remove(nameUsers);
+                        dictUsers.Add(nameUsers, pointSubmit);
+                    }
+                    //AddLanguage
+                    if (!dictLang.ContainsKey(language))
+                    {
+                        dictLang.Add(language, 0);
+                    }
+                    dictLang[language] += 1;
+                }
+                else
+                {
+                    var nameUsersToBand = command[0];
+                    if (dictUsers.ContainsKey(nameUsersToBand))
+                    {
+                        dictUsers.Remove(nameUsersToBand);
+                    }
+                }
+
+            }
+            //print
+            Console.WriteLine("Results:");
+            foreach (var kvp in dictUsers.OrderByDescending(x=>x.Value).ThenBy(x=>x.Key))
+            {
+                Console.WriteLine($"{kvp.Key} | {kvp.Value}");
+            }
+            Console.WriteLine("Submissions:");
+            foreach (var kvp in dictLang.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+            {
+                Console.WriteLine($"{kvp.Key} - {kvp.Value}");
+            }
+
+        }
+    }
+}

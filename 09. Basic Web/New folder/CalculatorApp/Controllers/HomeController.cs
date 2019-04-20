74888327f15747ac81fe10cbaa37de2d7992b9dd
vaglarov <pasific@abv.bg>
2019-04-20 19:00:31 +0300

    Basic WEB

---
+++ 09. Basic Web/New folder/CalculatorApp/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using CalculatorApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index(Calculator calculator)
+        {
+            return View(calculator);
+        }
+        [HttpPost]
+        public IActionResult Calculate(Calculator calculator)
+        {
+            calculator.CalculateResult();
+            if (calculator.RightOperand==0&&calculator.Operator=="/")
+            {
+                TempData["Error"] = "Can not divide by 0!";
+            }
+            else
+            {
+            OldCalculation.CalculatorHistory.Add(calculator);
+            }
+            return RedirectToAction("Index", calculator);
+        }
+        [HttpGet]
+        public IActionResult History()
+        {
+            return View(OldCalculation.CalculatorHistory);
+        }
+    }
+}
---
+++ 09. Basic Web/New folder/CalculatorApp/Models/OldCalculation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalculatorApp.Models
+{
+    public class OldCalculation
+    {
+        public static List<Calculator> CalculatorHistory { get; set; } = new List<Calculator>();
+    }
+}
